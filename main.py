8b74f6c0a6ae96bbfb98559111bf7592e47ee3ac
anyg2022 <109576101+anyg2022@users.noreply.github.com>
2022-08-04 16:37:42 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+view = [300, 200, 100, 500]
+st.write('# raw data')
+show_raw = st.checkbox('show raw data')
+if show_raw == True:
+    st.write('# table')
+    view
+
+st.table(view)
+st.write('# bar graph')
+st.bar_chart(view)
